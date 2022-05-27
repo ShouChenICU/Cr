d8d3a929839096f69c0e43cb9bab41813bd07fde
ShouChen <icu.996@foxmail.com>
2022-05-27 14:46:08 +0800

    新增：节点工厂工具类

---
+++ src/main/java/icu/mmmc/cr/utils/NodeFactory.java
@@ -0,0 +1,28 @@
+package icu.mmmc.cr.utils;
+
+import icu.mmmc.cr.constants.NodeAttributes;
+import icu.mmmc.cr.entities.NodeInfo;
+
+import java.security.KeyPair;
+import java.util.UUID;
+
+/**
+ * 节点工厂
+ *
+ * @author shouchen
+ */
+@SuppressWarnings("unused")
+public final class NodeFactory {
+    /**
+     * 生成一个节点信息
+     *
+     * @return 节点信息
+     */
+    public static NodeInfo genNodeInfo(String title, KeyPair keyPair) throws Exception {
+        return new NodeInfo()
+                .setUuid(UUID.nameUUIDFromBytes(keyPair.getPublic().getEncoded()).toString())
+                .setPublicKey(keyPair.getPublic())
+                .setAttr(NodeAttributes.$TITLE, title)
+                .sign(keyPair.getPrivate());
+    }
+}
