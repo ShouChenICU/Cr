2bf0a3973a3e345f15944f745352da4db911f227
ShouChen <icu.996@foxmail.com>
2022-06-09 16:58:00 +0800

    修复通讯协议版本匹配问题

--- src/main/java/icu/mmmc/cr/Version.java
+++ src/main/java/icu/mmmc/cr/Version.java
@@ -18,7 +18,7 @@ public final class Version {
     /**
      * 修订号
      */
-    public static final Integer PATCH_VERSION = 71;
+    public static final Integer PATCH_VERSION = 72;
     /**
      * 通讯协议版本
      */
